38fe3c1265bded382c1b64b99d3cf2c0904b6de9
Eoin Murray <eoin@kyso.io>
2023-04-25 23:00:08 +0200

    redirect to account if logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import { headers, cookies } from 'next/headers'
+import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { redirect } from 'next/navigation'
+import { Database } from '~/lib/database.types'
+
+export const revalidate = 0
 import IndexPage from './index-page'
 
-export default function Page() {
+export default async function Page() {
+  const supabase = createServerComponentSupabaseClient<Database>({
+    headers,
+    cookies,
+  })
 
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (session) {
+    console.log('Index: session exists, redirecting to /account')
+    return redirect('/account') 
+  }
+  
   return (
     <IndexPage />
   )
