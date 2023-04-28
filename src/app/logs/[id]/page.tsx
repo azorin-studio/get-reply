98fe01a9dffb337ebd1f4b143e55fa3f59cc4562
Eoin Murray <eoin@kyso.io>
2023-04-28 13:05:01 +0200

    removed extra log load on log page

--- src/app/logs/[id]/page.tsx
+++ src/app/logs/[id]/page.tsx
@@ -1,31 +1,9 @@
-import { headers, cookies } from "next/headers"
-import { createServerComponentSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '~/database.types'
-import { Log } from '~/types'
 import UnpureLogItem from '~/components/UnpureLogItem'
 
 export const revalidate = 0
 
 export default async function Page(props: { params: { id: string } }) {
   const id = props.params.id
-
-  const supabase = createServerComponentSupabaseClient<Database>({
-    headers,
-    cookies,
-  })
-
-  const { data: logs } = await supabase
-    .from('logs')
-    .select('*')
-    .eq('id', id)
-    .limit(1)
-
-  let log = null
-  if (logs && logs.length > 0) {
-    log = logs[0] as Log
-  }
-
-  if (!log) return <div>404</div>
 
   return (
     <div className="max-w-2xl mx-auto p-4 flex flex-row antialiased">
