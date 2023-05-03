7a3d5e7ac3b135ecd254b6aa8ae12e412155fafd
Eoin Murray <me@eoinmurray.eu>
2023-05-03 15:19:02 +0200

    problems come from next beta caching

--- src/app/api/cron/verify/route.ts
+++ src/app/api/cron/verify/route.ts
@@ -2,10 +2,15 @@ import { NextResponse } from "next/server"
 import { handleVerifyEvent } from "~/cron"
 
 export async function GET () {
+  console.log(1)
   try {
+    console.log(2)
     return NextResponse.json(await handleVerifyEvent())
+    console.log(3)
   } catch (error) {
+    console.log(4)
     console.error(error)
     return NextResponse.json({ error })
+    console.log(5)
   }
 }
