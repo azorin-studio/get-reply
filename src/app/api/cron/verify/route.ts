ca90876895b0432f7a0f5be1eef691c3c9ce6ed0
Eoin Murray <eoin@kyso.io>
2023-05-03 15:29:35 +0200

    removed redudant logs

--- src/app/api/cron/verify/route.ts
+++ src/app/api/cron/verify/route.ts
@@ -4,15 +4,10 @@ import { handleVerifyEvent } from "~/cron"
 export const revalidate = 0
 
 export async function GET () {
-  console.log(1)
   try {
-    console.log(2)
     return NextResponse.json(await handleVerifyEvent())
-    console.log(3)
   } catch (error) {
-    console.log(4)
     console.error(error)
     return NextResponse.json({ error })
-    console.log(5)
   }
 }
