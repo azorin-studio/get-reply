11f7b41e99ba16c46f5f2beef39ff93ddbab5de2
Eoin Murray <me@eoinmurray.eu>
2023-05-03 14:53:25 +0200

    more logs

--- src/app/api/cron/generate/route.ts
+++ src/app/api/cron/generate/route.ts
@@ -5,6 +5,7 @@ export async function GET () {
   try {
     return NextResponse.json(await handleGenerateEvent())
   } catch (error) {
+    console.error(error)
     return NextResponse.json({ error })
   }
 }
--- src/app/api/cron/verify/route.ts
+++ src/app/api/cron/verify/route.ts
@@ -5,6 +5,7 @@ export async function GET () {
   try {
     return NextResponse.json(await handleVerifyEvent())
   } catch (error) {
+    console.error(error)
     return NextResponse.json({ error })
   }
 }
