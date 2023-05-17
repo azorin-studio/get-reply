8b8b0af25de9286a639935f8b28d12bb4f56e77b
Eoin Murray <eoin@kyso.io>
2023-05-17 14:27:37 +0200

    checking logs

--- src/db/get-logs-by-status.ts
+++ src/db/get-logs-by-status.ts
@@ -4,6 +4,11 @@ import { Log } from "./types"
 
 export default async function getLogsByStatus (status: string) {
   console.log('getLogsByStatus', status)
+  console.log('getLogsByStatus', {
+    supabaseAdminClient,
+    SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  })
   const { error, data: logs } = await supabaseAdminClient
     .from('logs')
     .select()
