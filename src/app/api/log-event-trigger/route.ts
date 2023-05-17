48086e78106e8af35dc06d28925b0760d751dd30
Eoin Murray <eoin@kyso.io>
2023-05-17 14:08:16 +0200

    shorter log

--- src/app/api/log-event-trigger/route.ts
+++ src/app/api/log-event-trigger/route.ts
@@ -3,11 +3,10 @@ import { NextResponse } from 'next/server'
 import { handleAllEvents } from '~/cron/cron'
 
 export async function POST () {
-  console.log('log-event-trigger')
   if (headers().get('Authorization') !== `Bearer ${process.env.GETREPLY_BOT_AUTH_TOKEN}`) {
     return NextResponse.json({ error: 'Auth failed' })
   }
 
-  await handleAllEvents()
+  handleAllEvents()
   return NextResponse.json({ message: 'ok' })
 }
