cdc9af889d72c2fecadf526b798a51882ccfa073
Eoin Murray <me@eoinmurray.eu>
2023-06-11 11:04:20 +0200

    fixed create profile

--- src/app/callback/google/page.tsx
+++ src/app/callback/google/page.tsx
@@ -19,21 +19,24 @@ export default async function Page() {
   if(session) {
     const { error, data: profile } = await supabase
       .from('profiles')
-      .update({ google_refresh_token: session.provider_refresh_token })
+      .upsert({ 
+        google_refresh_token: session.provider_refresh_token,
+        email: session.user.email,
+        id: session.user.id,
+      })
       .eq('id', session.user.id)
       .select('*')
       .single()
 
     if (!profile) throw new Error('No profile found')
-    if (error) throw new Error(error.message)
-
-    // return redirect('/logs')
+    if (error) throw new Error((error as any).message)
+    return redirect('/logs')
   }
 
   return (
     <main className="flex-1 flex flex-col p-4 lg:px=2 gap-4">
       <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-        Google callback
+        Logging you in...
       </h2>
     </main>
   )
