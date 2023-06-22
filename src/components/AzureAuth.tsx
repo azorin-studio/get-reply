71af20dadb82a9f5651b8283784ee61524cb56ad
Eoin Murray <eoin@kyso.io>
2023-06-22 12:36:38 +0200

    changed azure button to microsoft button

--- src/components/AzureAuth.tsx
+++ src/components/AzureAuth.tsx
@@ -3,32 +3,37 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Auth } from '@supabase/auth-ui-react'
 import classNames from 'classnames'
 import { Database } from '~/db-admin/database.types'
+import MicrosoftLogo from './Microsoft-Logo'
 
 export default function AzureAuth() {
   const supabase = createClientComponentClient<Database>()
 
-  return (
-    <Auth 
-      supabaseClient={supabase} 
-      showLinks={false}
-      providers={['azure']}
-      redirectTo={`${process.env.NEXT_PUBLIC_SITE_URL}/callback`}
-      queryParams={{
-        access_type: 'offline',
+  async function signInWithAzure() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'azure',
+      options: {
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent'
+        },
         scopes: 'email offline_access',
-        prompt: 'consent',
-      }}
-      onlyThirdPartyProviders={true}
-      appearance={{
-        extend: false,
-        className: {
-          button: classNames(
-            "inline-flex flex-row gap-2 border items-center ",
-            "hover:bg-blue-50 focus:bg-slate-200 ",
-            "text-sm font-semibold text-[#3c4043] rounded px-3 py-2"
-          ),
-        }
-      }}
-    />
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/callback`
+      }
+    })
+
+  }
+
+  return (
+    <button
+      onClick={signInWithAzure}
+      className={classNames(
+        "inline-flex flex-row gap-2 border items-center ",
+        "hover:bg-blue-50 focus:bg-slate-200 ",
+        "text-sm font-semibold text-[#3c4043] rounded px-3 py-2"
+      )}
+    >
+      <MicrosoftLogo />
+      Sign in with Microsoft
+    </button>
   )
 }
