b54e7c080cb027ed9678dbf1c114edf5248d8e3d
Eoin Murray <me@eoinmurray.eu>
2023-06-30 14:18:33 +0200

    build on commit

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 
-import { serve } from "inngest/next";
-import { inngest } from "~/inngest/inngest";
-import { ingestEvents } from "~/inngest/events";
+import { serve } from "inngest/next"
+import { inngest } from "~/inngest/inngest"
+import { ingestEvents } from "~/inngest/events"
 
-export const { GET, POST, PUT } = serve(inngest, [...ingestEvents]);
+export const { GET, POST, PUT } = serve(inngest, [...ingestEvents])
