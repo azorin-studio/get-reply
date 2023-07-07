2de8e2a6b2558a446773997dc7b8e613eac34fd6
Eoin Murray <eoin@kyso.io>
2023-07-07 02:06:02 +0200

    added log to reminders

--- src/app/api/all-reminders/route.ts
+++ src/app/api/all-reminders/route.ts
@@ -4,7 +4,8 @@ import { eventBus } from '~/bus/event-list'
 export const revalidate = 0
 
 export async function POST () {
-  return NextResponse.json(
-    await eventBus.allReminders()
-  )
+  console.log(`+ ${new Date().toISOString()} all-reminders`)
+  const r = await eventBus.allReminders()
+  console.log(`- ${new Date().toISOString()} all-reminders`)
+  return NextResponse.json(r)
 }
