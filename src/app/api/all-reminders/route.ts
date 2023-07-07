e47068ad4f25463e8e3114273d0a7c01b37557eb
Eoin Murray <eoin@kyso.io>
2023-07-07 02:10:01 +0200

    cron is GET now

--- src/app/api/all-reminders/route.ts
+++ src/app/api/all-reminders/route.ts
@@ -3,7 +3,7 @@ import { eventBus } from '~/bus/event-list'
 
 export const revalidate = 0
 
-export async function POST () {
+export async function GET () {
   console.log(`+ ${new Date().toISOString()} all-reminders`)
   const r = await eventBus.allReminders()
   console.log(`- ${new Date().toISOString()} all-reminders`)
